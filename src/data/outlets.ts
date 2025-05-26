a633490a6d5af444507d530e2a6c39d2a617cac3
Lionel Murbach <lionel.murbach@selisegroup.com>
2025-05-26 11:27:26 +0200

    Update outlet names in outlets.ts for consistency and branding

--- src/data/outlets.ts
+++ src/data/outlets.ts
@@ -4,8 +4,8 @@ const orderUrl = 'https://app.ordermonkey.com/welcome/6447fb68-86a5-4448-ba4f-a5
 const orderUrlEur = 'https://app.ordermonkey.com/welcome/447b56fb-bf5d-416a-b5a3-205acde45222/25ccdda668e54b39acb59f854c2bf24b';
 
 export const outlets: FoodOutlet[] = [
-  { id: '1', name: 'WEBSITE DEMO', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80', isOpen: true, orderUrl },
-  { id: '2', name: 'WEBSITE DEMO (EUR)', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1659270157059-06aa84f64532?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', isOpen: true, orderUrl: orderUrlEur },
+  { id: '1', name: 'Website Demo', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80', isOpen: true, orderUrl },
+  { id: '2', name: 'Website Demo (EUR)', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1659270157059-06aa84f64532?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', isOpen: true, orderUrl: orderUrlEur },
   { id: '3', name: 'Momos', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1563245372-f21724e3856d?auto=format&fit=crop&w=400&q=80', isOpen: true, orderUrl },
   { id: '4', name: 'Sushi', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?auto=format&fit=crop&w=400&q=80', isOpen: true, orderUrl },
   { id: '5', name: 'Pizza', description: '', cuisineType: '', imageUrl: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?auto=format&fit=crop&w=400&q=80', isOpen: true, orderUrl },
